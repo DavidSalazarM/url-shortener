1e57c58748fc673dc7f85cba35d4347a5b450c7c
David Salazar <davidjosesalazarmora@gmail.com>
2021-11-26 19:38:52 -0500

    short

---
+++ urlreduce/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from . import models
+
+class ShortenerForm(forms.ModelForm):
+    long_url = forms.URLField(widget=forms.URLInput(
+        attrs={"class": "form-control", "placeholder": "Input the URL you want shortened"}))
+
+    class Meta:
+        model = models.Shortener
+        fields = ('long_url',)
+
---
+++ urlreduce/views.py
@@ -0,0 +1,57 @@
+from django import shortcuts
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+from .forms import ShortenerForm
+from .models import Shortener
+
+def home_view(request):
+
+    template = 'urlreduce/home.html'
+
+    context = {}
+
+    context['form'] = ShortenerForm()
+
+    if request.method == 'GET':
+        return render(request, template, context)
+
+    elif request.method == 'POST':
+        used_form = ShortenerForm(request.POST)
+        test_url = request.POST.get('long_url')
+
+        if used_form.is_valid():
+            try:
+                shortened_object = Shortener.objects.get(long_url=test_url)
+                context['times_followed'] = shortened_object.times_followed
+
+            except:
+                shortened_object = used_form.save()
+            
+            new_url = request.build_absolute_uri('/') + shortened_object.short_url
+
+            long_url = shortened_object.long_url
+
+            context['new_url'] = new_url
+            context['long_url'] = long_url
+
+            return render(request, template, context)
+
+        context['errors'] = used_form.errors
+
+        return render(request, template, context)
+
+def redirect_url_view(request, shortened_url):
+    try:
+        shortener = Shortener.objects.get(short_url=shortened_url)
+        shortener.times_followed += 1
+        shortener.save
+
+        return HttpResponseRedirect(shortener.long_url)
+
+    except:
+        raise Http404('Ouch! This link is broken') 
+    
+
+        
+
